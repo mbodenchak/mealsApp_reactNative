2c1045aeea5ffe65cae358459f6d838103763ca0
mbodenchak <mbodenchak@outlook.com>
2022-05-20 20:09:48 -0700

    fixed dumb c&p mistake

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,27 +1,27 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
-// import { useContext } from "react";
-// import { FavoritesContext } from "../store/context/favorites-context";
-import { MEALS } from "../data/dummy-data";
+// import { useContext } from 'react';
+
 import MealsList from "../components/MealsList/MealsList";
+// import { FavoritesContext } from '../store/context/favorites-context';
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
   // const favoriteMealsCtx = useContext(FavoritesContext);
-
   const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
   const favoriteMeals = MEALS.filter((meal) =>
     favoriteMealIds.includes(meal.id)
   );
-  //favoriteMealsCtx.Ids
 
   if (favoriteMeals.length === 0) {
     return (
-      <View styles={styles.rootContainer}>
-        <Text styles={styles.text}>You have no favorite meals yet.</Text>
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
       </View>
     );
   }
+
   return <MealsList items={favoriteMeals} />;
 }
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,41 +1,95 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
-// import { useContext } from 'react';
+import { useLayoutEffect } from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-import MealsList from "../components/MealsList/MealsList";
-// import { FavoritesContext } from '../store/context/favorites-context';
+import IconButton from "../components/IconButton";
+import List from "../components/MealDetail/List";
+import Subtitle from "../components/MealDetail/Subtitle";
+import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
+import { addFavorite, removeFavorite } from "../store/redux/favorites";
+// import { FavoritesContext } from '../store/context/favorites-context';
 
-function FavoritesScreen() {
+function MealDetailScreen({ route, navigation }) {
   // const favoriteMealsCtx = useContext(FavoritesContext);
   const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+  const dispatch = useDispatch();
 
-  const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealIds.includes(meal.id)
-  );
+  const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  if (favoriteMeals.length === 0) {
-    return (
-      <View style={styles.rootContainer}>
-        <Text style={styles.text}>You have no favorite meals yet.</Text>
-      </View>
-    );
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
+
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      // favoriteMealsCtx.removeFavorite(mealId);
+      dispatch(removeFavorite({ id: mealId }));
+    } else {
+      // favoriteMealsCtx.addFavorite(mealId);
+      dispatch(addFavorite({ id: mealId }));
+    }
   }
 
-  return <MealsList items={favoriteMeals} />;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon={mealIsFavorite ? "star" : "star-outline"}
+            color="white"
+            onPress={changeFavoriteStatusHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, changeFavoriteStatusHandler]);
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
 
-export default FavoritesScreen;
+export default MealDetailScreen;
 
 const styles = StyleSheet.create({
   rootContainer: {
-    flex: 1,
-    justifyContent: "center",
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
     alignItems: "center",
   },
-  text: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "white",
+  listContainer: {
+    width: "80%",
   },
 });
