916f3b96d3c9db2bff5871b51f193fd731a4943e
mbodenchak <mbodenchak@outlook.com>
2022-05-20 18:16:44 -0700

    fixed import in ln10 MealItem.js

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,3 +1,4 @@
+import { Text, View, StyleSheet } from "react-native";
 import { useContext } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
@@ -9,7 +10,28 @@ function FavoritesScreen() {
   const favoriteMeals = MEALS.filter((meal) =>
     favoriteMealsCtx.ids.includes(meal.id)
   );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View styles={styles.rootContainer}>
+        <Text styles={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
   return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
